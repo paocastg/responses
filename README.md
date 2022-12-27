ac734f8006ca345bd729fabc781b244c432cb466
anacastg <acastillo@keos.co>
2022-12-27 01:19:38 -0500

    [FIX] se agrega configuracion de api

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
-# README #
+# Cierre de conversación con tags
+Esta es una aplicación Rocket.Chat que está diseñada para cerrar conversaciones o chats con etiquetas, a través del comando de barra diagonal `/tagclose`. La aplicación requiere de API de carga de tags y API de cierre de conversación.
+# Instalación
 
-This README would normally document whatever steps are necessary to get your application up and running.
+1. npm install
+2. npm install -g @rocket.chat/apps-cli
 
-### What is this repository for? ###
+A continuación, deberá agregar la aplicación a su instancia de Rocket.Chat.
 
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+1. En la administración de Rocket.Chat, vaya a General -> Aplicaciones y habilite el modo de desarrollo para permitir la instalación de aplicaciones que no están en el mercado.
+2. Siga las instrucciones en https://rocket.chat/docs/developer-guides/developing-apps/getting-started/ para instalar apps-cli.
+3. Cambie el directorio al directorio de la aplicación e instale los módulos necesarios.
 
-### How do I get set up? ###
+```bash
+npm install
+```
 
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
+4. Implemente la aplicación en su laboratorio de instancias de Rocket.Chat o local.
+previamente configure el archivo .rcappsconfig con las credenciales de su usuario usado para el despliegue de aplicaciones desde su local indicando la url de su instancia de Rocket.Chat y el id de su usuario. En el caso no se tenga el ID y token, generarlo desde la plataforma de Rocketchat
 
-### Contribution guidelines ###
+```bash
+"url": "http://labdesk.keos.co:3000/",
+"userId":"5wjFAZyuFK8GtwgHo",
+```
 
-* Writing tests
-* Code review
-* Other guidelines
+```bash
+rc-apps deploy --token='xxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
+```
 
-### Who do I talk to? ###
+Si está modificando el código, puede ejecutar lo siguiente para actualizar la aplicación después de haberla instalado.
 
-* Repo owner or admin
-* Other community or team contact
+```bash
+rc-apps deploy --token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' --update
+```
+
+5. En la administración de Rocket.Chat, vaya a Aplicaciones -> Instalada ->Cierre de Chat con Tags  y habilite/configure con la API de carga de tags y API de cierre de conversación.
--- TagSelectRocketChatApp.ts
+++ TagSelectRocketChatApp.ts
@@ -15,9 +15,8 @@ import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/def
 import { IUIKitResponse, UIKitBlockInteractionContext, UIKitViewSubmitInteractionContext } from '@rocket.chat/apps-engine/definition/uikit';
 import { IUIKitContextualBarViewParam } from '@rocket.chat/apps-engine/definition/uikit/UIKitInteractionResponder';
 
-
-class CreateUiModal implements ISlashCommand {
-
+class TagsCommand implements ISlashCommand {
+    // this is what we will type when calling the slashcommand: /contextualbar
     public command = 'tagclose';
     public i18nParamsExample = 'tagselect';
     public i18nDescription = 'cierre de conversacion con etiquetas';
@@ -25,22 +24,18 @@ class CreateUiModal implements ISlashCommand {
 
     constructor(private readonly app: App) {}
 
-    public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-        const triggerId = context.getTriggerId() as string;
+    public async executor(context: SlashCommandContext, read: IRead, modify: IModify,http: IHttp): Promise<void> {
+        const triggerId = context.getTriggerId() as string; // [1]
+        const user = context.getSender();
         const settingsReader = read.getEnvironmentReader().getSettings();
-        const tags_list_api = await settingsReader.getValueById('tags_list_api');
-        const user = context.getSender()
+        const list_api = await settingsReader.getValueById('tags_list_api');
         const room:any = context.getRoom();
         const roomId = context.getRoom().id;
         const roomName= room.visitor.token;
 
-        /***Cargar lista de tags */
         let taglist : any[] = [];
 
-        const response = await http.get(tags_list_api);
-        console.log("user", user)
-        console.log("roomId", roomId)
-        console.log("room",roomName)
+        const response = await http.get(list_api);
 
         if(response){
             let  content: any = response.content;
@@ -50,14 +45,14 @@ class CreateUiModal implements ISlashCommand {
             }
         }
 
-        const contextualbarBlocks = await createContextualBarBlocks(modify, taglist, null,roomId, roomName , undefined); // [2]
+        const contextualbarBlocks = await createTagContextual(modify, taglist,roomId, roomName); // [2]
 
         await modify.getUiController().openContextualBarView(contextualbarBlocks, { triggerId }, user); // [3]
+
     }
 }
 
-async function createContextualBarBlocks(modify:IModify, taglist: any, tagSelect:any, roomId:any, roomName:any , viewId?:string ): Promise<IUIKitContextualBarViewParam>{
-
+function createTagContextual(modify: IModify, taglist: any, roomId:any, roomName:any ): IUIKitContextualBarViewParam {
     const block= modify.getCreator().getBlockBuilder();
 
     block.addActionsBlock({
@@ -78,7 +73,7 @@ async function createContextualBarBlocks(modify:IModify, taglist: any, tagSelect
     });
 
 
-    return {
+    return { // [6]
         id: roomId + "*" + roomName,
         title: block.newPlainTextObject('Cierre de conversación'),
         submit: block.newButtonElement({
@@ -89,13 +84,14 @@ async function createContextualBarBlocks(modify:IModify, taglist: any, tagSelect
     };
 }
 
+
 export class TagSelectRocketChatApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
+
     protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
-        configuration.slashCommands.provideSlashCommand(new CreateUiModal(this));
-        // se crean las variables de configuracion para el envio de mensajes
+        await configuration.slashCommands.provideSlashCommand(new TagsCommand(this));
         configuration.settings.provideSetting({
             id: 'tags_list_api',
             type: SettingType.STRING,
@@ -113,36 +109,32 @@ export class TagSelectRocketChatApp extends App {
             i18nLabel: 'URL API TagsClose',
         });
     }
-    public async executeBlockActionHandler(context: UIKitBlockInteractionContext ,  http: IHttp, modify: IModify,  read: IRead) {
 
+
+    public async executeBlockActionHandler(context: UIKitBlockInteractionContext, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify) {
         const data = context.getInteractionData();
         const settingsReader = read.getEnvironmentReader().getSettings();
-        const tags_list_api = await settingsReader.getValueById('tags_list_api');
+        const list_api = await settingsReader.getValueById('tags_list_api');
         const { actionId } = data;
-
+        var roomId:any;
+        var roomName:any;
 
         switch (actionId) {
             case 'changeTag': {
-                try{
-                    var taglist = Array<any>();
-                    const response = await http.get(tags_list_api);
-                    let tagSeleccionado = taglist.find(x => x.id === data.value);
-                    if(response){
-                        let  content: any = response.content;
-                        if (content !== undefined ) {
-                            content= JSON.parse(content);
-                            taglist = content.templates_info;
-                        }
-                    }
-                    const modal = await createContextualBarBlocks(modify,taglist, tagSeleccionado, "1122", "testando", undefined);
-                    return context.getInteractionResponder().updateModalViewResponse(modal);
 
-                } catch (err) {
-                    console.error(err);
-                    return {
-                        success: false,
-                    };
+                var taglist = Array<any>();
+                const response = await http.get(list_api);
+                let tagSeleccionado = taglist.find(x => x.id === data.value);
+                if(response){
+                    let  content: any = response.content;
+                    if (content !== undefined ) {
+                        content= JSON.parse(content);
+                        taglist = content.templates_info;
+                    }
                 }
+                const modal = await createTagContextual (modify, tagSeleccionado, roomId, roomName );
+                await modify.getUiController().updateContextualBarView(modal, { triggerId: data.triggerId }, data.user);
+
             }
         }
         return {
@@ -151,28 +143,25 @@ export class TagSelectRocketChatApp extends App {
     }
 
     // [10]
-    public async executeViewSubmitHandler(context: UIKitViewSubmitInteractionContext, modify: IModify, http: IHttp, read: IRead ): Promise<IUIKitResponse> {
-
+    public async executeViewSubmitHandler(context: UIKitViewSubmitInteractionContext, read:IRead, http:IHttp): Promise<IUIKitResponse> {
         const data = context.getInteractionData()
         const settingsReader = read.getEnvironmentReader().getSettings();
-        const tags_close_api = await settingsReader.getValueById('tags_close_api');
-        console.log("data", data)
+        const close_api = await settingsReader.getValueById('tags_close_api');
 
+
+        // [11]
         const { state }: {
             state: {
                 listTags: {
-                    changeTag: {
-                        selected_option: {
-                            value: string,
-                        },
-                    },
+                    changeTag: string,
+                    }
                 }
             }
-        } = data.view as any;
+         = data.view as any;
 
         let arrayData : any[] = data.view.id.split("*");
-
-        const response = await http.post(tags_close_api, {
+        // se hace el llamado del webhook ingresado desde la configuracion de la app en rocketchat
+        const response = await http.post(close_api, {
             headers: {
                 'Content-Type': 'application/json',
                 'charset': 'utf-8',
@@ -188,9 +177,10 @@ export class TagSelectRocketChatApp extends App {
             let  content: any = response.content;
             if (content !== undefined ) {
                 content= JSON.parse(content);
-                console.log("content", content);
+
             }
         }
+
         console.log("success tagclose");
         return context.getInteractionResponder().successResponse();
     }
